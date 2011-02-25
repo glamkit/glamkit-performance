8bf38673677b319fbe74fbdee861a8f3ab63ce7b
Thomas Ashelford <thomas@ether.com.au>
2011-02-24 22:27:53 -0800

    Made mediahash easier to switch off

--- mediahash/templatetags/mediahash.py
+++ mediahash/templatetags/mediahash.py
@@ -1,7 +1,9 @@
 import md5
 
 from django import template
+from django.conf import settings
 
+MEDIAHASH_INACTIVE = getattr(settings, 'MEDIAHASH_INACTIVE', settings.DEBUG)
 register = template.Library()
 
 @register.simple_tag
@@ -9,16 +11,10 @@ def mediahash(name):
 	return dohash(name)
 
 def dohash(name):
-	from django.conf import settings
-	if settings.DEBUG:
-		return "%s/%s" % (settings.MEDIA_URL, name)
-	else:
-		try:
-			from django.conf import settings
-		except ImportError:
-			return ''
-		m = md5.new()
+	if MEDIAHASH_ACTIVE:
 		m.update(name)
 		h = m.hexdigest()
 		number = int(h, 16) % settings.MEDIA_HASH_RANGE + 1
 		return "%s/%s" % (settings.MEDIA_HASH_URL % number, name)
+	else:
+		return "%s/%s" % (settings.MEDIA_URL, name)
